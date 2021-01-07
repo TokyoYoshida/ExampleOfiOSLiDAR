b2cf2a0215ca85fe62ec1ebd8f92d80e6e3a088d
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-01-07 17:53:51 +0900

    Initial Commit

---
+++ ExampleOfiOSLiDAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ExampleOfiOSLiDAR
+//
+//  Created by TokyoYoshida on 2021/01/07.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
