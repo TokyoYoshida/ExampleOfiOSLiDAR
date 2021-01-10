7a8248dbd8718bc1045288e7b204b9eb28156955
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-01-10 16:28:44 +0900

    add mesh

--- ExampleOfiOSLiDAR/ViewController.swift
+++ ExampleOfiOSLiDAR/ViewController.swift
@@ -5,20 +5,36 @@
 //  Created by TokyoYoshida on 2021/01/07.
 //
 
-import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        arView.session.delegate = self
+
         
         // Add the box anchor to the scene
+
+        arView.environment.sceneUnderstanding.options = []
+        arView.environment.sceneUnderstanding.options.insert(.occlusion)
+        arView.environment.sceneUnderstanding.options.insert(.physics)
+        arView.debugOptions.insert(.showSceneUnderstanding)
+        arView.renderOptions = [.disablePersonOcclusion, .disableDepthOfField, .disableMotionBlur]
+        arView.automaticallyConfigureSession = false
+
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.sceneReconstruction = .meshWithClassification
+
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
         arView.scene.anchors.append(boxAnchor)
     }
 }
