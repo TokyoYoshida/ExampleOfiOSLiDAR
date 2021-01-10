91be7c9cf5bfa0cd342a5d23a97f37e10cf00083
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-01-10 16:30:59 +0900

    add sphere

--- ExampleOfiOSLiDAR/ViewController.swift
+++ ExampleOfiOSLiDAR/ViewController.swift
@@ -33,8 +33,27 @@ class ViewController: UIViewController, ARSessionDelegate {
         configuration.environmentTexturing = .automatic
         arView.session.run(configuration)
 
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        arView.addGestureRecognizer(tapRecognizer)
+
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         arView.scene.anchors.append(boxAnchor)
+    }
+
+    @objc
+    func handleTap(_ sender: UITapGestureRecognizer) {
+        func sphere(radius: Float, color: UIColor) -> ModelEntity {
+            let sphere = ModelEntity(mesh: .generateSphere(radius: radius), materials: [SimpleMaterial(color: color, isMetallic: false)])
+            // Move sphere up by half its diameter so that it does not intersect with the mesh
+            sphere.position.y = radius
+            return sphere
+        }
+        let tapLocation = sender.location(in: arView)
+        if let result = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any).first {
+            let resultAnchor = AnchorEntity(world: result.worldTransform)
+            resultAnchor.addChild(sphere(radius: 0.1, color: .lightGray))
+            arView.scene.addAnchor(resultAnchor)
+        }
     }
 }
