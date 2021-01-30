2441587efaaea1e33438a6a4f15b33377df2d819
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-01-30 16:13:54 +0900

    refactor

--- ExampleOfiOSLiDAR/DepthMapViewController.swift
+++ ExampleOfiOSLiDAR/DepthMapViewController.swift
@@ -24,64 +24,24 @@ class DepthMapViewController: UIViewController, ARSessionDelegate {
     }()
 
     override func viewDidLoad() {
+        func buildConfigure() -> ARWorldTrackingConfiguration {
+            let configuration = ARWorldTrackingConfiguration()
+
+            configuration.environmentTexturing = .automatic
+            if type(of: configuration).supportsFrameSemantics(.sceneDepth) {
+               configuration.frameSemantics = .sceneDepth
+            }
+
+            return configuration
+        }
         super.viewDidLoad()
         
         arView.session.delegate = self
-
-        
-        // Add the box anchor to the scene
-
-        arView.environment.sceneUnderstanding.options = []
-        arView.environment.sceneUnderstanding.options.insert(.occlusion)
-        arView.environment.sceneUnderstanding.options.insert(.physics)
-//        arView.debugOptions.insert(.showSceneUnderstanding)
-        arView.renderOptions = [.disablePersonOcclusion, .disableDepthOfField, .disableMotionBlur]
-        arView.automaticallyConfigureSession = false
-
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.sceneReconstruction = .meshWithClassification
-
-        configuration.environmentTexturing = .automatic
-        if type(of: configuration).supportsFrameSemantics(.sceneDepth) {
-           // Activate sceneDepth
-           configuration.frameSemantics = .sceneDepth
-        }
+        let configuration = buildConfigure()
         arView.session.run(configuration)
-
-        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        arView.addGestureRecognizer(tapRecognizer)
-
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        arView.scene.anchors.append(boxAnchor)
     }
 
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
-       guard let sceneDepth = frame.smoothedSceneDepth ?? frame.sceneDepth  else { return }
-        var pixelBuffer: CVPixelBuffer!
-        pixelBuffer = sceneDepth.depthMap
-        
         imageView.image = session.currentFrame?.depthMapTransformedImage(orientation: orientation, viewPort: self.imageView.bounds)
-        
-
-//        var texturePixelFormat: MTLPixelFormat!
-//        setMTLPixelFormat(&texturePixelFormat, basedOn: pixelBuffer)
-//        depthTexture = createTexture(fromPixelBuffer: pixelBuffer, pixelFormat: texturePixelFormat, planeIndex: 0)
-    }
-
-    @objc
-    func handleTap(_ sender: UITapGestureRecognizer) {
-        func sphere(radius: Float, color: UIColor) -> ModelEntity {
-            let sphere = ModelEntity(mesh: .generateSphere(radius: radius), materials: [SimpleMaterial(color: color, isMetallic: false)])
-            // Move sphere up by half its diameter so that it does not intersect with the mesh
-            sphere.position.y = radius
-            return sphere
-        }
-        let tapLocation = sender.location(in: arView)
-        if let result = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any).first {
-            let resultAnchor = AnchorEntity(world: result.worldTransform)
-            resultAnchor.addChild(sphere(radius: 0.1, color: .lightGray))
-            arView.scene.addAnchor(resultAnchor)
-        }
     }
 }
