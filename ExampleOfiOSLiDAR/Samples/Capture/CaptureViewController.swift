cae7fda0fa75b2afd4f3e09d9b7a02b5d870143d
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-11 15:06:14 +0900

    export mesh

--- ExampleOfiOSLiDAR/Samples/Capture/CaptureViewController.swift
+++ ExampleOfiOSLiDAR/Samples/Capture/CaptureViewController.swift
@@ -58,12 +58,41 @@ class CaptureViewController: UIViewController, ARSessionDelegate {
     
     @objc
     func handleTap(_ sender: UITapGestureRecognizer) {
-        guard let frame = arView.session.currentFrame else {return}
-        guard let device = MTLCreateSystemDefaultDevice() else {
-                    return
-                }
-                
-        let meshAnchors = frame.anchors.compactMap({ $0 as? ARMeshAnchor }) as [ARMeshAnchor]
-        print(meshAnchors[0].geometry.toMDLMesh(device: device))
+        exportMesh()
+//        guard let frame = arView.session.currentFrame else {return}
+//        guard let device = MTLCreateSystemDefaultDevice() else {
+//                    return
+//                }
+//
+//        let meshAnchors = frame.anchors.compactMap({ $0 as? ARMeshAnchor }) as [ARMeshAnchor]
+//        print(meshAnchors[0].geometry.toMDLMesh(device: device))
+    }
+    
+    func exportMesh() {
+        let meshAnchors = arView.session.currentFrame?.anchors.compactMap({ $0 as? ARMeshAnchor });
+
+        DispatchQueue.global().async {
+
+            let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0];
+            let filename = directory.appendingPathComponent("MyFirstMesh.obj");
+
+            guard let device = MTLCreateSystemDefaultDevice() else {
+                print("metal device could not be created");
+                return;
+            };
+
+            let asset = MDLAsset();
+
+            for anchor in meshAnchors! {
+                let mdlMesh = anchor.geometry.toMDLMesh(device: device);
+                asset.add(mdlMesh);
+            }
+
+            do {
+                try asset.export(to: filename);
+            } catch {
+                print("failed to write to file");
+            }
+        }
     }
 }
