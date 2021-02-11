f116981cccb2a8da90b1c42c712852df92765a05
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-11 15:33:40 +0900

    rafactor

--- ExampleOfiOSLiDAR/Samples/Capture/CaptureViewController.swift
+++ ExampleOfiOSLiDAR/Samples/Capture/CaptureViewController.swift
@@ -43,55 +43,44 @@ class CaptureViewController: UIViewController, ARSessionDelegate {
             let configuration = buildConfigure()
             arView.session.run(configuration)
         }
-        func addGesture() {
-            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-            arView.addGestureRecognizer(tapRecognizer)
-        }
         arView.session.delegate = self
         super.viewDidLoad()
         initARView()
-        addGesture()
     }
 
-    func session(_ session: ARSession, didUpdate frame: ARFrame) {
-    }
-    
-    @objc
-    func handleTap(_ sender: UITapGestureRecognizer) {
-//        exportMesh()
-//        guard let frame = arView.session.currentFrame else {return}
-//        guard let device = MTLCreateSystemDefaultDevice() else {
-//                    return
-//                }
-//
-//        let meshAnchors = frame.anchors.compactMap({ $0 as? ARMeshAnchor }) as [ARMeshAnchor]
-//        print(meshAnchors[0].geometry.toMDLMesh(device: device))
-    }
-    
-    @IBAction func exportMesh(_ sender: UIButton) {
-        let meshAnchors = arView.session.currentFrame?.anchors.compactMap({ $0 as? ARMeshAnchor });
-
-        DispatchQueue.main.async {
-
-            let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-            let url = directory.appendingPathComponent("scaned.obj")
-
-            guard let device = MTLCreateSystemDefaultDevice() else {return}
+    @IBAction func tappedExportButton(_ sender: UIButton) {
+        func convertToAsset(meshAnchors: [ARMeshAnchor]) -> MDLAsset? {
+            guard let device = MTLCreateSystemDefaultDevice() else {return nil}
 
             let asset = MDLAsset()
 
-            for anchor in meshAnchors! {
+            for anchor in meshAnchors {
                 let mdlMesh = anchor.geometry.toMDLMesh(device: device)
                 asset.add(mdlMesh)
             }
+            
+            return asset
+        }
+        func export(asset: MDLAsset) throws -> URL {
+            let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let url = directory.appendingPathComponent("scaned.obj")
 
+            try asset.export(to: url)
+
+            return url
+        }
+        func share(url: URL) {
+            let vc = UIActivityViewController(activityItems: [url],applicationActivities: nil)
+            vc.popoverPresentationController?.sourceView = sender
+            self.present(vc, animated: true, completion: nil)
+        }
+        if let meshAnchors = arView.session.currentFrame?.anchors.compactMap({ $0 as? ARMeshAnchor }),
+           let asset = convertToAsset(meshAnchors: meshAnchors) {
             do {
-                try asset.export(to: url)
-                let vc = UIActivityViewController(activityItems: [url],applicationActivities: nil)
-                vc.popoverPresentationController?.sourceView = sender
-                self.present(vc, animated: true, completion: nil)
+                let url = try export(asset: asset)
+                share(url: url)
             } catch {
-                print("failed")
+                print("export error")
             }
         }
     }
