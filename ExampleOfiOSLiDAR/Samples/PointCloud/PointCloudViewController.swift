6e40087d22e3bab472434992b847cdd820ed15f6
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-07-23 06:46:45 +0900

    switch to Metal rendering

--- ExampleOfiOSLiDAR/Samples/PointCloud/PointCloudViewController.swift
+++ ExampleOfiOSLiDAR/Samples/PointCloud/PointCloudViewController.swift
@@ -5,44 +5,112 @@
 //  Created by TokyoYoshida on 2021/01/07.
 //
 
-import RealityKit
 import ARKit
+import Metal
+import MetalKit
+import CoreImage
 
-class PointCloudViewController: UIViewController, ARSessionDelegate {
+class PointCloudViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet weak var mtkView: MTKView!
     
-    @IBOutlet var arView: ARView!
-    @IBOutlet weak var imageView: UIImageView!
-    
+    // ARKit
+    private var session: ARSession!
+    var alphaTexture: MTLTexture?
+
+    // Metal
+    private let device = MTLCreateSystemDefaultDevice()!
+    private var commandQueue: MTLCommandQueue!
+    private var matteGenerator: ARMatteGenerator!
+    lazy private var textureCache: CVMetalTextureCache = {
+        var cache: CVMetalTextureCache?
+        CVMetalTextureCacheCreate(kCFAllocatorDefault, nil, device, nil, &cache)
+        return cache!
+    }()
+
+    private var texture: MTLTexture!
+    lazy private var renderer = SimpleShaderRenderer(device: device)
+
+
     var orientation: UIInterfaceOrientation {
         guard let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation else {
             fatalError()
         }
         return orientation
     }
-    @IBOutlet weak var imageViewHeight: NSLayoutConstraint!
-    lazy var imageViewSize: CGSize = {
-        CGSize(width: view.bounds.size.width, height: imageViewHeight.constant)
-    }()
 
     override func viewDidLoad() {
-        func buildConfigure() -> ARWorldTrackingConfiguration {
+        func initMatteGenerator() {
+            matteGenerator = ARMatteGenerator(device: device, matteResolution: .half)
+        }
+        func loadTexture() {
+            let textureLoader = MTKTextureLoader(device: device)
+            texture = try! textureLoader.newTexture(name: "fuji", scaleFactor: view.contentScaleFactor, bundle: nil)
+            mtkView.colorPixelFormat = texture.pixelFormat
+            mtkView.framebufferOnly = false
+        }
+        func initMetal() {
+            commandQueue = device.makeCommandQueue()
+            mtkView.device = device
+            mtkView.framebufferOnly = false
+            mtkView.delegate = self
+        }
+        func runARSession() {
             let configuration = ARWorldTrackingConfiguration()
 
-            configuration.environmentTexturing = .automatic
-            if type(of: configuration).supportsFrameSemantics(.sceneDepth) {
-               configuration.frameSemantics = .sceneDepth
-            }
+            configuration.frameSemantics = .personSegmentationWithDepth
 
-            return configuration
+            session.run(configuration)
+        }
+        func initARSession() {
+            session = ARSession()
+            runARSession()
+        }
+        func createTexture() {
+            let width = mtkView.currentDrawable!.texture.width
+            let height = mtkView.currentDrawable!.texture.height
+
+            let colorDesc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: mtkView.colorPixelFormat,
+                                                                 width: height, height: width, mipmapped: false)
+            colorDesc.usage = MTLTextureUsage(rawValue: MTLTextureUsage.renderTarget.rawValue | MTLTextureUsage.shaderRead.rawValue)
+
         }
         super.viewDidLoad()
-        
-        arView.session.delegate = self
-        let configuration = buildConfigure()
-        arView.session.run(configuration)
+        initARSession()
+        initMatteGenerator()
+        initMetal()
+        createTexture()
+    }
     }
 
-    func session(_ session: ARSession, didUpdate frame: ARFrame) {
-        imageView.image = session.currentFrame?.depthMapTransformedImage(orientation: orientation, viewPort: self.imageView.bounds)
+extension PointCloudViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+    
+    func draw(in view: MTKView) {
+        func getAlphaTexture(_ commandBuffer: MTLCommandBuffer) -> MTLTexture? {
+            guard let currentFrame = session.currentFrame else {
+                return nil
+            }
+
+            return matteGenerator.generateMatte(from: currentFrame, commandBuffer: commandBuffer)
+        }
+        func buildRenderEncoder(_ commandBuffer: MTLCommandBuffer) -> MTLRenderCommandEncoder? {
+            let rpd = view.currentRenderPassDescriptor
+            rpd?.colorAttachments[0].clearColor = MTLClearColorMake(0, 1, 0, 1)
+            rpd?.colorAttachments[0].loadAction = .clear
+            rpd?.colorAttachments[0].storeAction = .store
+            return commandBuffer.makeRenderCommandEncoder(descriptor: rpd!)
+        }
+        guard let drawable = view.currentDrawable else {return}
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+
+        renderer.update(commandBuffer, drawable: drawable)
+                
+        commandBuffer.present(drawable)
+        
+        commandBuffer.commit()
+        
+        commandBuffer.waitUntilCompleted()
     }
 }
---
+++ ExampleOfiOSLiDAR/Samples/PointCloud/SimpleShaderRenderer.swift
@@ -0,0 +1,60 @@
+//
+//  Renderer.swift
+//  ARKitExamples
+//
+//  Created by TokyoYoshida on 2021/07/19.
+//
+
+import ARKit
+import Metal
+import MetalKit
+import CoreImage
+
+class SimpleShaderRenderer {
+    private let device: MTLDevice
+    private var renderPipeline: MTLRenderPipelineState!
+
+    private var vertextBuffer: MTLBuffer!
+    private let vertexData: [Float] = [
+        -1, -1, 0, 1,
+         1, -1, 0, 1,
+        -1,  1, 0, 1,
+         1,  1, 0, 1,
+    ]
+
+    init(device: MTLDevice) {
+        func buildPipeline() {
+            guard let library = device.makeDefaultLibrary() else {fatalError()}
+            let descriptor = MTLRenderPipelineDescriptor()
+            descriptor.vertexFunction = library.makeFunction(name: "simpleVertexShader")
+            descriptor.fragmentFunction = library.makeFunction(name: "simpleFragmentShader")
+            descriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+            renderPipeline = try! device.makeRenderPipelineState(descriptor: descriptor)
+        }
+        func buildBuffers() {
+            let size = vertexData.count * MemoryLayout<Float>.size
+            vertextBuffer = device.makeBuffer(bytes: vertexData, length: size)
+        }
+
+        self.device = device
+        buildPipeline()
+        buildBuffers()
+    }
+    
+    func update(_ commandBuffer: MTLCommandBuffer, drawable: CAMetalDrawable) {
+        
+        let renderPassDescriptor: MTLRenderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+
+        guard let renderPipeline = renderPipeline else {fatalError()}
+
+        
+        renderEncoder.setRenderPipelineState(renderPipeline)
+        renderEncoder.setVertexBuffer(vertextBuffer, offset: 0, index: 0)
+        renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
+
+        renderEncoder.endEncoding()
+    }
+}
